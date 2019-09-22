4596c153a59dcea53c53a63b9ab5a13430c20aef
rafaroman18 <rafaroman182@gmail.com>
2019-09-23 00:25:17 +0200

    starting scripts and Drive connections

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const commandArgs = require('./arguments')
 
 const bot = new Telegraf('889371432:AAHj8cosL9FY8ngcLkd6vrD7n3h9HYsXjC0')
 
-bot.use(commandArgs)
+bot.use(commandArgs) //Divide the message chat into raw/command/args
 
 bot.start((ctx) => ctx.reply('Pablo Tonto, tonto, tontisimo.'))
 bot.help((ctx) => ctx.reply('Te voy a meter un guantazo que vamos a morir los dos. Tu del guantazo y yo de la onda expansiva'))
