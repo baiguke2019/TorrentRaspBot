18002cdb5f41545c93e1b0e6455ed8babcfa5399
rafaroman18 <rafaroman182@gmail.com>
2019-09-22 19:59:33 +0200

    bot working

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const Telegraf = require('telegraf')
+const commandArgs = require('./arguments')
 
 const bot = new Telegraf('889371432:AAHj8cosL9FY8ngcLkd6vrD7n3h9HYsXjC0')
 
+bot.use(commandArgs)
 
 bot.start((ctx) => ctx.reply('Pablo Tonto, tonto, tontisimo.'))
 bot.help((ctx) => ctx.reply('Te voy a meter un guantazo que vamos a morir los dos. Tu del guantazo y yo de la onda expansiva'))
