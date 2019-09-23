b5716752eb42664bcf3aab41422af2be4ba745fa
rafaroman18 <rafaroman182@gmail.com>
2019-09-23 12:02:15 +0200

    script writing😎"

--- script_Download.sh
+++ script_Download.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-wget -O Super_Secret_File  $1 #We download the file and check what is
-mv Super_Secret_File ~
-VAR=$(file -b ~/Super_Secret_File)
-echo $VAR
+wget -O Super_Secret_File  $1 #We download the file
+mv Super_Secret_File ~  #We move to home
+VAR=$(file -b ~/Super_Secret_File) #Assigned the type of file to VAR
+if VAR=="BitTorrent"
+    then
+    #####Starting Transmission Commands 
+    echo Es Torrent
+fi
+if [-d "~/mnt/gdrive/TorrentRaspBotDownloads"];
+    then mkdir ~/mnt/gdrive/TorrentRaspBotDownloads
+fi
+mv ~/Super_Secret_File ~/mnt/gdrive/TorrentRaspBotDownloads
