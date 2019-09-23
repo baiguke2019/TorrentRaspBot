6451c2dc23fc1e382330014086f7094e270ad422
rafaroman18 <rafaroman182@gmail.com>
2019-09-23 16:53:30 +0200

    adding status messages on chat

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bot = new Telegraf('889371432:AAHj8cosL9FY8ngcLkd6vrD7n3h9HYsXjC0')
 bot.use(commandArgs) //Divide the message chat into raw/command/args
 var shell = require('shelljs')
 
-bot.start((ctx) => shell.exec('bash prueba.sh')
+bot.start((ctx) => shell.exec('bash prueba.sh'))
 bot.help((ctx) => ctx.reply('Te voy a meter un guantazo que vamos a morir los dos. Tu del guantazo y yo de la onda expansiva'))
 
 bot.launch()
