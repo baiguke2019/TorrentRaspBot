ebaaafefa9ad51a1623207b08f761f0932174963
rafaroman18 <rafaroman182@gmail.com>
2019-09-23 17:30:39 +0200

    almost workingg 🐱‍👓

--- download.js
+++ download.js
@@ -1,8 +1,14 @@
 var shell = require('shelljs')
 
 const download = (ctx) => {
-    if(shell.exec('bash prueba.sh')){
-        ctx.reply('FUNCIONA')
+    if(ctx.command.args.length != 1)
+    {
+        ctx.reply('ERROR in arguments. Please introduce 1 and only 1 link')
+    }
+    else{
+        if(shell.exec('bash script_Download.sh ',ctx.command.args[0])){
+            ctx.reply('DESCARGANDO')
+        }
     }
 }
 
