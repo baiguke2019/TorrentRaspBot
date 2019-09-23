b6d09dcb8eecf8b171409fcd7f4232014e8ccc1d
rafaroman18 <rafaroman182@gmail.com>
2019-09-23 17:53:36 +0200

    download command WORKSgit add . 😎

--- script_Download.sh
+++ script_Download.sh
@@ -2,11 +2,11 @@
 wget -O Super_Secret_File $1 #We download the file
 mv Super_Secret_File ~  #We move to home
 VAR=$(file -b ~/Super_Secret_File) #Assigned the type of file to VAR
-if VAR=="BitTorrent"
+if $VAR=="BitTorrent"
     then
     #####Starting Transmission Commands 
     echo Es Torrent
 fi
-mkdir ~/mnt/gdrive/TorrentRaspBotDownloads
+mkdir -p ~/mnt/gdrive/TorrentRaspBotDownloads
 
 mv ~/Super_Secret_File ~/mnt/gdrive/TorrentRaspBotDownloads
