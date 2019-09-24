10adb7fabb5d6b3021c24e33e7ded2d1a2cca214
rafaroman18 <rafaroman182@gmail.com>
2019-09-24 09:00:08 +0200

    Writing Transmission Script 👾

--- download.js
+++ download.js
@@ -6,8 +6,16 @@ const download = (ctx) => {
         ctx.reply('ERROR in arguments. Please introduce 1 and only 1 link')
     }
     else{
+        ctx.reply('Downloading...')
         if(shell.exec('bash script_Download.sh ' + ctx.command.args[0])){
-            ctx.reply('DESCARGANDO')
+        ctx.reply('Downloaded.')
+            if(shell.exec('file -b ~/Super_Secret_File')=='BitTorrent File'){
+                ctx.reply('Detected BitTorrent File. Starting Transmission.')
+                shell.exec('bash script_Torrent.sh')
+            }
+        }
+        else{
+            ctx.reply('UNKNOWN ERROR. It wasn\'t possible to download the file. ')
         }
     }
 }
--- script_Download.sh
+++ script_Download.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 wget -O Super_Secret_File $1 #We download the file
 mv Super_Secret_File ~  #We move to home
-VAR=$(file -b ~/Super_Secret_File) #Assigned the type of file to VAR
-if $VAR=="BitTorrent"
-    then
-    #####Starting Transmission Commands 
-    echo Es Torrent
-fi
-mkdir -p ~/mnt/gdrive/TorrentRaspBotDownloads
 
-mv ~/Super_Secret_File ~/mnt/gdrive/TorrentRaspBotDownloads
