0c221bf8584a739adf5f733dd78d56f25b2452f2
rafaroman18 <rafaroman182@gmail.com>
2019-09-24 13:38:52 +0200

    Doing testssss🐍

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Telegraf = require('telegraf')
 const commandArgs = require('./arguments')
 const download = require('./download')
+var shell = require('shelljs')
 
 const bot = new Telegraf('889371432:AAHj8cosL9FY8ngcLkd6vrD7n3h9HYsXjC0')
 
@@ -11,5 +12,6 @@ bot.start((ctx) => ctx.reply('Welcome to TorrentRaspBot! \nWrite /help to see th
 bot.help((ctx) => ctx.reply('These are the available commands:\n/download "file url" -> Downloads the file and automatically uploads it to Google Drive\n/author -> Shows the author and some info of him'))
 bot.command('author',(ctx) => ctx.reply('Author: Rafael Roman \nGithub: github.com/rafaroman18 \n2019'))
 bot.command('download',download)
+bot.command('test',shell.exec('sudo bash prueba_download.sh' + ctx.command.args[0]))
 
 bot.launch()
